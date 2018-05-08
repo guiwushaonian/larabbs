6612d5b421b086fbef1f2e84f8798e9ec6d356bf
gxh <519996283@qq.com>
2018-05-08 05:48:48 +0000

    only authorized user can edit proflie

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -9,6 +9,11 @@ use Illuminate\Http\Request;
 
 class UsersController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth', ['except' => ['show']]);
+    }
+
     // show
     public function show(User $user)
     {
@@ -18,12 +23,17 @@ class UsersController extends Controller
     // edit
     public function edit(User $user)
     {
+        // 验证授权策略
+        $this->authorize('update', $user);
+
         return view('users.edit', compact('user'));
     }
 
     // update
     public function update(UserRequest $request, ImageUploadHandler $uploader, User $user)
     {
+        $this->authorize('update', $user);
+
         $data = $request->all();
         if ($request->avatar) {
             $result = $uploader->save($request->avatar, 'avatars', $user->id, 362);
