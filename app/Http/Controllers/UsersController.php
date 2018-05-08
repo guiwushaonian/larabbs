b142e22f2427c194687162c8839a490b8e90b0af
gxh <519996283@qq.com>
2018-05-08 03:56:54 +0000

    add user profile page

---
+++ app/Http/Controllers/UsersController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class UsersController extends Controller
+{
+    // show
+    public function show(User $user)
+    {
+        return view('users.show', compact('user'));
+    }
+}
