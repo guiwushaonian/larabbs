4c534b11602a417da41e43e81e475d63c1f4cccd
gxh <519996283@qq.com>
2018-05-08 04:44:14 +0000

    basic user profile edit

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\UserRequest;
 use App\Models\User;
 use Illuminate\Http\Request;
 
@@ -11,5 +12,18 @@ class UsersController extends Controller
     public function show(User $user)
     {
         return view('users.show', compact('user'));
+    }
+
+    // edit
+    public function edit(User $user)
+    {
+        return view('users.edit', compact('user'));
+    }
+
+    // update
+    public function update(UserRequest $request, User $user)
+    {
+        $user->update($request->all());
+        return redirect()->route('users.show', $user->id)->with('success', '个人资料更新成功');
     }
 }
