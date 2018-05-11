c5934b878cf02fd398bc7901d18bce6648b15487
gxh <519996283@qq.com>
2018-05-11 09:16:24 +0000

    List topics created by user

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -47,6 +47,7 @@ class UsersTableSeeder extends Seeder
         $user->name = 'david';
         $user->email = 'gaoxuhui2011@163.com';
         $user->avatar = 'https://fsdhubcdn.phphub.org/uploads/images/201710/14/1/ZqM7iaP4CR.png?imageView2/1/w/200/h/200';
+        $user->password = bcrypt('password');
         $user->save();
     }
 }
